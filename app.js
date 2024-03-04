2e5489f295ab880831b3a3a5f07909ed8f26bb51
Shivamshivpriye <133257312+Shivamshivpriye@users.noreply.github.com>
2024-03-04 13:29:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const router=require("./Routes/router");
 const PORT = process.env.PORT || 6010
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-app.use(cors());
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/uploads",express.static("./uploads"));
 app.use("/files",express.static("/public/files"))
